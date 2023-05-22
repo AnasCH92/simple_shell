bc3470d70decd5deb26a11d9c70fd8ded77edc22
Anas Chakir <anas_chakir@outlook.com>
2023-05-22 15:30:30 -0700

    betty adjustement

--- functions.c
+++ functions.c
@@ -10,17 +10,17 @@
 
 #include <unistd.h>
 
-void _printf(const char *str)
-{
-    if (str == NULL)
-        return;
-
-    while (*str != '\0')
-    {
-        write(STDOUT_FILENO, str, 1);
-        str++;
-    }
-}
+	void _printf(const char *str)
+	{
+    	if (str == NULL)
+     	return;
+	
+   	while (*str != '\0')
+    	{
+       	write(STDOUT_FILENO, str, 1);
+       	str++;
+   	}
+	}
 
 /**
  * init_data - init data
@@ -29,12 +29,12 @@ void _printf(const char *str)
  * Return: void
  */
 
-void init_data(input *data, const char *shell_name)
-{
+	void init_data(input *data, const char *shell_name)
+	{
 	data->command = NULL;
 	data->av = NULL;
 	data->shell_name = shell_name;
-}
+	}
 
 /**
  * start_process - start a new process
@@ -42,32 +42,32 @@ void init_data(input *data, const char *shell_name)
  * Return: void
  */
 
-void child_process(input *data, char **environ)
-{
-    pid_t child_pid = fork();
-    int status = 0;
-
-    if (child_pid == -1)
-        goto free;
-    if (child_pid == 0 && execve(data->av[0], data->av, environ) == -1)
-		goto free;
-    else if (wait(&status) == -1)
-		goto free;
+	void child_process(input *data, char **environ)
+	{	
+   	pid_t child_pid = fork();
+   	int status = 0;
+	
+ 	if (child_pid == -1)
+    	goto free;
+  	if (child_pid == 0 && execve(data->av[0], data->av, environ) == -1)
+	goto free;
+  	else if (wait(&status) == -1)
+	goto free;
 	return;
-free:
+	free:
 	perror(data->shell_name);
 	free_array(data->av);
 	free(data->command);
 	exit(EXIT_FAILURE);
-}
+	}
 /**
  * command_line - get the commend from the prompt and structure it into data struct
  * @d: data struct input
  * Return: void
  */
 
-void command_line(input *data)
-{
+	void command_line(input *data)
+	{
 	size_t n = 0;
 	ssize_t nb_of_chars;
 
@@ -75,12 +75,12 @@ void command_line(input *data)
 
 	if (nb_of_chars == -1)
 	{
-		free(data->command);
-		exit(EXIT_FAILURE);
+	free(data->command);
+	exit(EXIT_FAILURE);
 	}
 
 	data->command[nb_of_chars - 1] = '\0';
-}
+	}
 
 /**
  * handler_sigint - Signal handler function
@@ -88,12 +88,12 @@ void command_line(input *data)
  * Return: void
  */
 
-void _sigint(int signal)
-{
+	void _sigint(int signal)
+	{
 	const char prompt[] = "\n#csisfun$ ";
 	(void)signal;
 	_printf(prompt);
-}
+	}
 
 /**
  * split - split a given string by a delimiter
@@ -102,17 +102,17 @@ void _sigint(int signal)
  * Return: void
  */
 
-void command_spliter(input *data, const char *delim)
-{
-    char *token;
+	void command_spliter(input *data, const char *delim)
+	{
+   	char *token;
 	int ntoken = 0;
 
 	data->av = malloc(2 * sizeof(char *));
 	if (data->av == NULL)
 	{
-		free(data->command);
-		perror(data->shell_name);
-		exit(EXIT_FAILURE);
+	free(data->command);
+	perror(data->shell_name);
+	exit(EXIT_FAILURE);
 	}
 	data->av[0] = NULL;
 	data->av[1] = NULL;
@@ -120,21 +120,21 @@ void command_spliter(input *data, const char *delim)
 	token = strtok(data->command, delim);
 	while (token)
 	{
-		data->av = _realloc(data->av, (ntoken + 2) * sizeof(char *));
-		if (data->av == NULL)
-			goto free;
-		data->av[ntoken] = _strdup(token);
-		if (data->av[ntoken] == NULL)
-			goto free;
-		ntoken++;
-		token = strtok(NULL, delim);
+	data->av = _realloc(data->av, (ntoken + 2) * sizeof(char *));
+	if (data->av == NULL)
+	goto free;
+	data->av[ntoken] = _strdup(token);
+	if (data->av[ntoken] == NULL)
+	goto free;
+	ntoken++;
+	token = strtok(NULL, delim);
 	}
 	data->av[ntoken] = NULL;
 	return;
-free:
+	free:
 	free_array(data->av);
 	free(data->command);
 	perror(data->shell_name);
 	exit(EXIT_FAILURE);
 
-}
+	}
