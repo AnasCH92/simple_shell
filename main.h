87c0299b47bbf63b3578a8367712540c8252866c
Anas Chakir <anas_chakir@outlook.com>
2023-05-23 08:51:27 -0700

    task update betty

--- main.h
+++ main.h
@@ -8,48 +8,48 @@
 #include <string.h>
 #include <sys/stat.h>
 
-extern char **environ;
-
-
-
-typedef struct input
-{
-    char **av;
-    char *command;
-    const char *shell_name;
-} input;
+	extern char **environ;
+	
+	
+	
+	typedef struct input
+	{
+	char **av;
+	char *command;
+	const char *shell_name;
+	}input;
 
 /**
  * struct builtin - holds the main data.
  * @cmd: built in cmd
  * @f: function of builtin cmd
  */
-typedef struct builtin
-{
+	typedef struct builtin
+	{
 	const char *command;
 	void (*f)(input *data);
-} builtin;
+	} builtin;
 
 
 
-void _printf(const char *str);
-void command_line(input *d);
-void init_data(input *data, const char *shell_name);
-void command_spliter(input *data, const char *delim);
-void free_array(char **array);
-void child_process(input *data, char **environ);
-int exec_builtin(input *data);
-void builtin_exit(input *data);
-void builtin_env(input *data);
-void _sigint(int signal);
-char **_realloc(char **ptr, size_t size);
-char	*_strdup(const char *s1);
-void    command_path(input *data);
-int	_strlen(char	*str);
-char	*_strcpy(char	*dest, char	*src);
-char	*_strcat(char	*dest, char	*src);
-int	_strcmp(char	*s1, char	*s2);
-char *get_env(char *env_var);
-int _atoi(char *s);
+	void _printf(const char *str);
+	void command_line(input *d);
+	void init_data(input *data, const char *shell_name);
+	void command_spliter(input *data, const char *delim);
+	void free_array(char **array);
+	void child_process(input *data, char **environ);
+	int exec_builtin(input *data);
+	void builtin_exit(input *data);
+	void builtin_env(input *data);
+	void _sigint(int signal);
+	char **_realloc(char **ptr, size_t size);
+	char	*_strdup(const char *s1);
+	void    command_path(input *data);
+	int	_strlen(char	*str);
+	char	*_strcpy(char	*dest, char	*src);
+	char	*_strcat(char	*dest, char	*src);
+	int	_strcmp(char	*s1, char	*s2);
+	char *get_env(char *env_var);
+	int _atoi(char *s);
 
 #endif
