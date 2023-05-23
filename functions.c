a9e87ec6eb5fa2e3b93221e94978f8cc634cbc6f
mouadia <ouadia.mohammed@outlook.de>
2023-05-23 23:46:36 +0200

    update

--- functions.c
+++ functions.c
@@ -1,30 +1,31 @@
 #include "main.h"
 
 /**
- * _putchar - writes the char c tou stdout.
- * @c: the char to print
- * Return: 1 for succes and -1 for error. 
- */
+* _putchar - writes the char c tou stdout.
+* @c: the char to print
+* Return: 1 for succes and -1 for error. 
+*/
 int	_putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 /**
- * _putchar - Writes the character c to stdout
+ * _printf - Writes string to stdout
  *
- * @str: The character to print
+ * @s1: string to print
  *
  * Return: On success 1.
 */
-
-#include <unistd.h>
 
 void _printf(const char *s1)
 {
 	int i = 0;
 
 	while (s1[i] != '\0')
+	{
 		_putchar(s1[i]);
+		i++;
+	}
 	_putchar('\n');
 }
 
